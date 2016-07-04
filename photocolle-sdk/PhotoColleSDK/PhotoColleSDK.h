88bdf58b8f6c9fe2d7c134d6902c1b3f45c2fa59
rizakii <riza.alaudin@kii.com>
2016-07-04 13:13:12 +0900

    Rearrange projects structure.
    - make cocoa touch framework target
    - remove unused targets

---
+++ photocolle-sdk/PhotoColleSDK/PhotoColleSDK.h
@@ -0,0 +1,19 @@
+//
+//  PhotoColleSDK.h
+//  PhotoColleSDK
+//
+//  Created by syahRiza on 7/4/16.
+//  Copyright © 2016 奈良 信介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoColleSDK.
+FOUNDATION_EXPORT double PhotoColleSDKVersionNumber;
+
+//! Project version string for PhotoColleSDK.
+FOUNDATION_EXPORT const unsigned char PhotoColleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoColleSDK/PublicHeader.h>
+
+
