44e64c1efa7477192dc3d2ffdc8a9113d9dab723
rizakii <riza.alaudin@kii.com>
2016-07-04 16:10:32 +0900

    add umbrella header.
    Add sdk archive.

--- photocolle-sdk/PhotoColleSDK/PhotoColleSDK.h
+++ photocolle-sdk/PhotoColleSDK/PhotoColleSDK.h
@@ -14,6 +14,37 @@ FOUNDATION_EXPORT double PhotoColleSDKVersionNumber;
 //! Project version string for PhotoColleSDK.
 FOUNDATION_EXPORT const unsigned char PhotoColleSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PhotoColleSDK/PublicHeader.h>
 
-
+#import <PhotoColleSDK/DCAuthenticationContext.h>
+#import <PhotoColleSDK/DCAuthority.h>
+#import <PhotoColleSDK/DCCapacityInfo.h>
+#import <PhotoColleSDK/DCContentBodyInfo.h>
+#import <PhotoColleSDK/DCContentGUID.h>
+#import <PhotoColleSDK/DCContentGUIDListResponse.h>
+#import <PhotoColleSDK/DCContentInfo.h>
+#import <PhotoColleSDK/DCContentInfoListResponse.h>
+#import <PhotoColleSDK/DCContentThumbnailInfo.h>
+#import <PhotoColleSDK/DCContentThumbnailInfoList.h>
+#import <PhotoColleSDK/DCDTO.h>
+#import <PhotoColleSDK/DCDataID.h>
+#import <PhotoColleSDK/DCDateFiltering.h>
+#import <PhotoColleSDK/DCDetailedContentInfo.h>
+#import <PhotoColleSDK/DCDetailedContentInfoListResponse.h>
+#import <PhotoColleSDK/DCEnumerations.h>
+#import <PhotoColleSDK/DCErrors.h>
+#import <PhotoColleSDK/DCEventTag.h>
+#import <PhotoColleSDK/DCFavoriteTag.h>
+#import <PhotoColleSDK/DCListResponse.h>
+#import <PhotoColleSDK/DCModifiedDateFilter.h>
+#import <PhotoColleSDK/DCNamedTagHead.h>
+#import <PhotoColleSDK/DCPersonTag.h>
+#import <PhotoColleSDK/DCPhotoColle.h>
+#import <PhotoColleSDK/DCPhotoColleInternalAccessor.h>
+#import <PhotoColleSDK/DCPhotoColleSDK.h>
+#import <PhotoColleSDK/DCPlacementTag.h>
+#import <PhotoColleSDK/DCScope.h>
+#import <PhotoColleSDK/DCTag.h>
+#import <PhotoColleSDK/DCTagHead.h>
+#import <PhotoColleSDK/DCTagListResponse.h>
+#import <PhotoColleSDK/DCUploadDateFilter.h>
+#import <PhotoColleSDK/DCYearMonthTag.h>
---
+++ photocolle-sdk/sdk-archive.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Check git path
+GIT_PATH=$(which git)
+GIT_BASE="../.git"
+DIST_DIR="dist"
+OLD_ARCHIVE_FILE="./*.zip"
+
+# Check git
+if [ ! -d ${GIT_BASE} ] || [ -z ${GIT_PATH} ]; then
+    echo "Git repository is not exist!"
+    exit 1;
+fi
+
+# Check target directory
+if [ ! -d ${DIST_DIR} ]; then
+    echo "Dist directory was not found. Please compile SDK first."
+    exit 2;
+fi
+
+# Get branch name / CommitID
+if [ -z $GIT_BRANCH_NAME ]; then
+    GIT_BRANCH_NAME=$(git branch | grep -e '^\*' | sed -e 's/^\* //g')
+fi
+GIT_BRANCH_NAME=`echo $GIT_BRANCH_NAME | sed -e 's/[\/ ]/_/g'`
+GIT_COMMIT_ID=$(git rev-list -n 1 --abbrev-commit HEAD)
+FRAMEWORK_ARCHIVE_NAME="KiiSDK.framework-${GIT_BRANCH_NAME}-${GIT_COMMIT_ID}.zip"
+DSYMS_ARCHIVE_NAME="KiiSDK.framework.dSYMs-${GIT_BRANCH_NAME}-${GIT_COMMIT_ID}.zip"
+
+# Create SDK archive
+if [ -d ${DIST_DIR} ]; then
+    if [ -e ${OLD_ARCHIVE_FILE} ]; then
+        rm -r ${OLD_ARCHIVE_FILE}
+    fi
+    cd ${DIST_DIR}
+    zip -ry "${FRAMEWORK_ARCHIVE_NAME}" "PhotoColleSDK.framework"
+    zip -ry "${DSYMS_ARCHIVE_NAME}" "dSYMs"
+    echo SDK_BODY_FILE:KiiSDK-Private-v2/${DIST_DIR}/${FRAMEWORK_ARCHIVE_NAME} > ci-publish.prop
+    cd ../
+else
+    echo "${DIST_DIR} not exist!"
+    exit 2;
+fi
